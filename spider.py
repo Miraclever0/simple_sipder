14bd56e4add0b6d7c0418d92ac153b3639b905f1
Miraclever0 <maverick_lu@foxmail.com>
2024-03-26 10:38:30 +0800

    use multiple threads to accelerate crawler process

--- spider.py
+++ spider.py
@@ -4,10 +4,14 @@ import re
 from queue import *
 from save2table import saveExcel,saveCsv
 import json
+import threading
+# from tqdm import tqdm
 
 url_queue = Queue()
+detail_queue = Queue()
 result = []
 repeat_set = set()
+lock = threading.Lock()
 
 def getHtml(url):
     html = 0
@@ -69,25 +73,28 @@ def remove_duplicate(temp_list):
             new_list.append(temp)
     return new_list
 
-# def formatDetails(src_dict, trg_dict):
-#     for key, value in src_dict.items():
-#         if key not in trg_dict:
-#             trg_dict[key] = [value]
-#         else:
-#             trg_dict[key].append(value)
-
 def formatDetails(src_list, trg_dict):
     for temp_dict in src_list:
         for key in temp_dict.keys():
             trg_dict[key] = []
-    # for key, value in trg_dict.items():
-    #     print("key: {}, value: {}".format(key, value))
     for temp_dict in src_list:
         for key, value in trg_dict.items():
             if key in temp_dict:
                 trg_dict[key].append(temp_dict[key])
             else:
                 trg_dict[key].append("")
+
+def getResult():
+    detail_url = ""
+    while(detail_queue.qsize() > 0):
+        with lock:
+            detail_url = detail_queue.get()
+        if detail_url != "":
+            print(detail_url)
+            detail_html = getHtml(detail_url)
+            temp_dict = getDetails(detail_html)
+            result.append(temp_dict)
+    return
 
 if __name__ == "__main__":
     url = "https://www.chinatruck.org/product/s0_b0_t0_m0_e0_c1.html"
@@ -108,22 +115,16 @@ if __name__ == "__main__":
         print("next_page: ", next_page)
 
     detail_list = remove_duplicate(temp_list)
-    count = 0
     for detail_page in detail_list:
-        print(count)
-        detail_html = getHtml(detail_page)
-        temp_dict = getDetails(detail_html)
-        result_list.append(temp_dict)
-        count += 1
+        detail_queue.put(detail_page)
 
-    formatDetails(result_list, result_dict)
+    threads = []
+    for i in range(0,10):
+        t = threading.Thread(target=getResult)
+        t.start()
+        threads.append(t)
 
-    # with open("out.json", 'w') as fw:
-    #     fw.write(json.dumps(result_dict, ensure_ascii=False))
-
-
-    # result_dict = {}
-    # with open("out.json", 'r') as fr:
-    #     result_dict = json.loads(fr.read())
+    for t in threads:
+        t.join()
+    formatDetails(result, result_dict)
     saveCsv(result_dict, 'output.csv')
-    # print(result_dict)
